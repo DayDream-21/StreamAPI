c90e464662493ae05678d81096235914ce1ba180
DayDream-21 <vova13158@gmail.com>
2022-01-28 18:23:38 +0300

    Create class Person

---
+++ src/main/java/com/slavamashkov/Person.java
@@ -0,0 +1,41 @@
+package com.slavamashkov;
+
+public class Person {
+    enum Position {
+        ENGINEER, DIRECTOR, MANAGER
+    }
+
+    private String name;
+    private int age;
+    private Position position;
+
+    public Person(String name, int age, Position position) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
