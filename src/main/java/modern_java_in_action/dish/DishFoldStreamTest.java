4c0c537b933f7c0b5ca457582f54ec59a5b70627
DayDream-21 <vova13158@gmail.com>
2022-04-05 22:12:59 +0300

    Test folding stream methods
     * anyMatch
     * allMatch
     * findAny
     * findFirst

---
+++ src/main/java/modern_java_in_action/dish/DishFoldStreamTest.java
@@ -0,0 +1,38 @@
+package modern_java_in_action.dish;
+
+import java.util.List;
+
+public class DishFoldStreamTest {
+    public static void main(String[] args) {
+        List<Dish> menu = new Dish().initializeMenu();
+
+        System.out.println("Avg calories per dish: " +
+                avgCaloriesPerDish(menu));
+
+        System.out.println("Dish with max calories: " +
+                maxCalories(menu));
+
+        System.out.println("Amount of dishes: " +
+                amountOfDishes(menu));
+    }
+
+    private static int avgCaloriesPerDish(List<Dish> menu) {
+        int totalCalories = menu.stream()
+                .map(Dish::getCalories)
+                .reduce(0, (a, b) -> a + b);
+
+        return totalCalories / menu.size();
+    }
+
+    private static int maxCalories(List<Dish> menu) {
+         return menu.stream()
+                .map(Dish::getCalories)
+                .reduce(0, Integer::max);
+    }
+
+    private static int amountOfDishes(List<Dish> menu) {
+        return menu.stream()
+                .map(d -> 1)
+                .reduce(0, Integer::sum);
+    }
+}
