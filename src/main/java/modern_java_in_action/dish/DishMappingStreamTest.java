3048c6000b81d9595d900bc4a2605f0b3b640097
DayDream-21 <vova13158@gmail.com>
2022-04-05 15:49:29 +0300

    Test mapping stream methods
     * map
     * flatMap

---
+++ src/main/java/modern_java_in_action/dish/DishMappingStreamTest.java
@@ -0,0 +1,49 @@
+package modern_java_in_action.dish;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DishMappingStreamTest {
+    public static void main(String[] args) {
+        List<Dish> menu = new Dish().initializeMenu();
+
+        System.out.println("Dishes name: "
+                + getDishesName(menu));
+
+        System.out.println("Dishes ordered by name length: "
+                + getDishesOrderedByNamesLength(menu));
+
+        List<Integer> ints = List.of(1, 2, 3, 4, 5);
+        List<Integer> squares = ints.stream()
+                .map(integer -> integer * integer)
+                .collect(Collectors.toList());
+
+        System.out.println("Squares: " + squares);
+
+        List<Integer> first = List.of(1, 2, 3);
+        List<Integer> second = List.of(3, 4);
+
+        List<int[]> pairs = first.stream()
+                .flatMap(i -> second.stream()
+                        .filter(j -> (i + j) % 3 == 0)
+                        .map(j -> new int[] {i, j}))
+                .collect(Collectors.toList());
+
+        pairs.forEach(p -> System.out.println(Arrays.toString(p)));
+    }
+
+    private static List<String> getDishesName(List<Dish> menu) {
+        return menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getDishesOrderedByNamesLength(List<Dish> menu) {
+        return menu.stream()
+                .map(Dish::getName)
+                .sorted(Comparator.comparing(String::length))
+                .collect(Collectors.toList());
+    }
+}
