3e2250c128a0bbdb34c1612e283d93c93ba1daf6
DayDream-21 <vova13158@gmail.com>
2022-04-05 15:51:26 +0300

    Create method initializeMenu
    Method initializes default menu, so user shouldn't copy-paste same menu
    each time

--- src/main/java/modern_java_in_action/dish/Dish.java
+++ src/main/java/modern_java_in_action/dish/Dish.java
@@ -1,16 +1,36 @@
 package modern_java_in_action.dish;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Dish {
-    private final String name;
-    private final boolean vegetarian;
-    private final int calories;
-    private final Type type;
+    private String name;
+    private boolean vegetarian;
+    private int calories;
+    private Type type;
+
+    public Dish() {
+    }
 
     public Dish(String name, boolean vegetarian, int calories, Type type) {
         this.name = name;
         this.vegetarian = vegetarian;
         this.calories = calories;
         this.type = type;
+    }
+
+    public List<Dish> initializeMenu() {
+        return Arrays.asList(
+                new Dish("pork",         false, 800, Dish.Type.MEAT),
+                new Dish("beef",         false, 700, Dish.Type.MEAT),
+                new Dish("chicken",      false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true,  530, Dish.Type.OTHER),
+                new Dish("rice",         true,  350, Dish.Type.OTHER),
+                new Dish("season fruit", true,  120, Dish.Type.OTHER),
+                new Dish("pizza",        true,  550, Dish.Type.OTHER),
+                new Dish("prawns",       false, 300, Dish.Type.FISH),
+                new Dish("salmon",       false, 450, Dish.Type.FISH)
+        );
     }
 
     public String getName() {
