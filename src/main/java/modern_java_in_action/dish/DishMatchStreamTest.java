5008268b9672ee2d9e34fedf84181ab3c6a0b40d
DayDream-21 <vova13158@gmail.com>
2022-04-05 22:11:55 +0300

    Test matching stream methods
     * anyMatch
     * allMatch
     * findAny
     * findFirst

---
+++ src/main/java/modern_java_in_action/dish/DishMatchStreamTest.java
@@ -0,0 +1,38 @@
+package modern_java_in_action.dish;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class DishMatchStreamTest {
+    public static void main(String[] args) {
+        List<Dish> menu = new Dish().initializeMenu();
+
+        System.out.println("Is menu partially vegetarian? " +
+                isMenuPartiallyVegetarian(menu));
+        System.out.println("Is menu healthy (all dishes has less then 1000 cal.)? " +
+                isMenuHealthy(menu));
+
+        menu.stream()
+                .filter(Dish::isVegetarian)
+                .findAny()
+                .ifPresent(dish -> System.out.println(dish.getName()));
+
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                        .map(n -> n * n)
+                        .filter(n -> n % 3 == 0)
+                        .findFirst(); // Результат равен 9
+    }
+
+    private static boolean isMenuPartiallyVegetarian(List<Dish> menu) {
+        return menu.stream()
+                .anyMatch(Dish::isVegetarian);
+    }
+
+    private static boolean isMenuHealthy(List<Dish> menu) {
+        return menu.stream()
+                .allMatch(dish -> dish.getCalories() < 1000);
+    }
+}
