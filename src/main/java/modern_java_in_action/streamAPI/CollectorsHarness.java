6452e5a98736194568812aae5f72db22827f0607
DayDream-21 <vova13158@gmail.com>
2022-04-18 20:15:16 +0300

    Test custom and build-in collectors

---
+++ src/main/java/modern_java_in_action/streamAPI/CollectorsHarness.java
@@ -0,0 +1,19 @@
+package modern_java_in_action.streamAPI;
+
+public class CollectorsHarness {
+    public static void main(String[] args) {
+        long fastest = Long.MAX_VALUE;
+
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            PartitionStream.partitionPrimesWithCustomCollector(1_000_000);
+            //PartitionStream.partitionPrimes(1_000_000);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            if (duration < fastest) {
+                fastest = duration;
+            }
+        }
+
+        System.out.println("Fastest time completion: " + fastest + " millis");
+    }
+}
